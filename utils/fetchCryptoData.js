697889d24e94ccc5bd606ff98be52f8c1cc68bce
KhushiSaini7 <94100598+KhushiSaini7@users.noreply.github.com>
2025-01-10 23:43:04 +0530

    add utility function to calculate standard deviation for crypto prices

---
+++ utils/fetchCryptoData.js
@@ -0,0 +1,8 @@
+function calculateStandardDeviation(values) {
+    const mean = values.reduce((sum, val) => sum + val, 0) / values.length;
+    const variance = values.reduce((sum, val) => sum + Math.pow(val - mean, 2), 0) / values.length;
+    return Math.sqrt(variance);
+  }
+  
+  module.exports = { calculateStandardDeviation };
+  
